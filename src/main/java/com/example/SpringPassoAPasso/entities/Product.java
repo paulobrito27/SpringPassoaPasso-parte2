4f590507431bd6941bb6ca99288e269c7e1d57e7
paulobrito27 <paulojmbrito27@gmail.com>
2020-04-30 10:46:52 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/example/SpringPassoAPasso/entities/Product.java
+++ src/main/java/com/example/SpringPassoAPasso/entities/Product.java
@@ -10,7 +10,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "Tb_produtos")
@@ -27,6 +30,9 @@ public class Product {
 	@ManyToMany
 	@JoinTable(name = "product_x_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> itens = new HashSet<>();
 
 	public Product() {
 
@@ -87,6 +93,15 @@ public class Product {
 
 	public void addCategories(Category categoria) {
 		this.categories.add(categoria);
+	}
+	
+	@JsonIgnore 
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem item : itens) {
+			set.add(item.getOrder());
+		}
+		return set;
 	}
 
 	@Override
